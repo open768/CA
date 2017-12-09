4b47188190e1e295e0a2215501c98b39e7fd1e82
open seven six ate <open768@yahoo.co.uk>
2017-12-09 23:53:50 +0000

    reorganised privates

--- js/widgets/cacanvas.js
+++ js/widgets/cacanvas.js
@@ -15,10 +15,14 @@ $.widget( "ck.cacanvas",{
 		cols:100,
 		rows:100,
 		cell_size:5,
-		oGrid: null,
-		oCanvas:null,
 		white_image:"images/whitebox.png",
-		black_image:"images/blackbox.png"
+		black_image:"images/blackbox.png",
+		_privates:{
+			oGrid: null,
+			oCanvas:null,
+			bDrawing:false,
+			iImageCount:0
+		}
 	},
 	
 	//#################################################################
@@ -39,7 +43,7 @@ $.widget( "ck.cacanvas",{
 		
 		//associate a CA grid with the widget
 		var oGrid = new cCAGrid(oOptions.rows, oOptions.cols);
-		oOptions.oGrid = oGrid;
+		oOptions._privates.oGrid = oGrid;
 		bean.on(oGrid, "done", function(){oThis.onGridDone()});
 		
 		//put something in the widget
@@ -53,13 +57,15 @@ $.widget( "ck.cacanvas",{
 	onCAEvent: function( poEvent){
 		var oThis = this;
 		var oOptions = oThis.options;
+		var oPrivOptions = oOptions._privates;
+		
 		
 		switch (poEvent.type){
 			case cCAConsts.event_types.set_rule:
-				oOptions.oGrid.rule = poEvent.data;
+				oPrivOptions.oGrid.rule = poEvent.data;
 				break;
 			case cCAConsts.event_types.initialise:
-				oOptions.oGrid.init(poEvent.data);
+				oPrivOptions.oGrid.init(poEvent.data);
 				break;
 		}
 	},
@@ -69,35 +75,54 @@ $.widget( "ck.cacanvas",{
 		this.pr__drawGrid();
 	},
 	
+	//****************************************************************
+	onImageLoad:function(){
+		var oThis = this;
+		var oOptions = oThis.options;
+		var oPrivOptions = oOptions._privates;
+		
+		oPrivOptions.iImageCount --;
+		if (! oPrivOptions.bDrawing && (oPrivOptions.iImageCount ==0)){
+			cDebug.write("finished drawing");
+		}
+		
+	},
+	
 	//#################################################################
 	//# privates
 	//#################################################################`
 	pr__initCanvas: function(){
 		var oThis = this;
 		var oOptions = oThis.options;
+		var oPrivOptions = oOptions._privates;
 		var oElement = oThis.element;
+
 		
 		//create the html5 canvas to draw on
 		oElement.empty();
 		var oCanvas = $("<canvas>");
 		oCanvas.attr("width",oOptions.cols*oOptions.cell_size);
 		oCanvas.attr("height",oOptions.rows*oOptions.cell_size);
-		oOptions.oCanvas = oCanvas;
+		oPrivOptions.oCanvas = oCanvas;
 		oElement.append(oCanvas);
 				
 		//fill the canvas with a pretty random pattern
-		oOptions.oGrid.init(cCAConsts.init_values.block);
+		oPrivOptions.oGrid.init(cCAConsts.init_values.block);
 		
 		//link the cells in the grid
-		oOptions.oGrid.link_cells(cCAConsts.neighbours.eightway);
+		oPrivOptions.oGrid.link_cells(cCAConsts.neighbours.eightway);
 	},
+	
 	
 	//****************************************************************
 	pr__drawGrid: function(){
 		var oThis = this;
 		var oOptions = oThis.options;
-		var oCanvas = oOptions.oCanvas;
-		var oGrid = oOptions.oGrid;
+		var oPrivOptions = oOptions._privates;
+		var oCanvas = oPrivOptions.oCanvas;
+		var oGrid = oPrivOptions.oGrid;
+		oPrivOptions.iImageCount = 0;
+		oPrivOptions.bDrawing = true;
 		
 		oCanvas.clearCanvas();
 		var y=0;
@@ -106,11 +131,18 @@ $.widget( "ck.cacanvas",{
 			for (var ic=1; ic<= oGrid.cols; ic++){
 				var oCell = oGrid.getCell(ir,ic);
 				var sImg = (oCell.value==0?oOptions.white_image:oOptions.black_image);
-				oCanvas.drawImage({  source: sImg, x: x, y: y,fromCenter:false});
+				oCanvas.drawImage({  
+					source: sImg, 
+					x: x, y: y,fromCenter:false, 
+					load:function(){	oThis.onImageLoad();	}
+				});
+				oPrivOptions.iImageCount++;
 				x+= oOptions.cell_size;
 			}
 			y+= oOptions.cell_size;
 		}
+		oPrivOptions.bDrawing = false;
 	}	
+	
 	
 });
