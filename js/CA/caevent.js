15263dbb062dded9777960edc26406800f9c42b3
sun-van-work <sunil.vanmullem@cgi.com>
2026-02-06 10:14:21 +0000

    added event debug

--- js/CA/caevent.js
+++ js/CA/caevent.js
@@ -36,7 +36,7 @@ class cCABaseEvent {
 		if (!psGridName || !psAction) $.error('incorrect number of arguments')
 		if (typeof bean === 'undefined') $.error('bean library is missing')
 
-		// @ts-expect-error
+		if (this.constructor === cCABaseEvent) $.error('cCABaseEvent is abstract')
 		if (!this.constructor.event_type_id) $.error('event_type_id not overridden in class:' + this.constructor.name)
 
 		this.grid_name = psGridName
@@ -55,28 +55,32 @@ class cCABaseEvent {
 
 	async trigger() {
 		var sEventName = this.channel_id()
+		cDebug.write('event>> grid:"' + this.grid_name + '" type:' + this.constructor.name + ' action:' + this.action)
 		bean.fire(document, sEventName, this)
 	}
 
 	static async fire_event(psGridName, psAction, poData = null) {
-		var oEvent = new this(psGridName, psAction, poData)
+		if (this === cCABaseEvent) throw new CAException('cCABaseEvent is abstract')
+		var oEvent = new this(psGridName, psAction, poData) //create specific instance
 		oEvent.trigger()
 	}
 
 	static async subscribe(psGridName, pfnCallback) {
+		if (this === cCABaseEvent) throw new CAException('cCABaseEvent is abstract')
 		var oEvent = new this(psGridName, 'dummy') //create an event to get the channel ID
 		bean.on(document, oEvent.channel_id(), pfnCallback)
 	}
 }
 
 //***************************************************************************
-
+// subclasses of cCABaseEvent for specific event types - these are the events that will be fired and listened for in the app
+//***************************************************************************
 class cCAActionEvent extends cCABaseEvent {
 	static event_type_id = 'CAACTEV'
 	static actions = {
-		ready: 'AERD',
-		grid_init: 'AEGI',
-		control: 'AECN'
+		ready: 'AAready',
+		grid_init: 'AAinit',
+		control: 'AAcontrol'
 	}
 }
 
@@ -85,8 +89,8 @@ class cCAActionEvent extends cCABaseEvent {
 class cCARuleEvent extends cCABaseEvent {
 	static event_type_id = 'CARULEEV'
 	static actions = {
-		update_rule: 'REUR',
-		set_rule: 'GESR'
+		update_rule: 'RAupdate',
+		set_rule: 'RAset'
 	}
 }
 
@@ -111,8 +115,8 @@ class cCACanvasEvent extends cCABaseEvent {
 	static event_type_id = 'CACANVASEV'
 	static actions = {
 		grid_status: 'CAstatus',
-		set_grid: 'CASetgrid',
-		import_grid: 'GEIG'
+		set_grid: 'CAset',
+		import_grid: 'CAimport'
 	}
 	static notify = {
 		nochange: 'CNnochange'
