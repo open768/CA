9e7500d458563e408cc5621563b4e2ea8b2a017c
sun-van-work <sunil.vanmullem@cgi.com>
2026-03-12 15:25:44 +0000

    prep work

--- js/CA/scrambler/op_reader.js
+++ js/CA/scrambler/op_reader.js
@@ -1,45 +1,12 @@
 "use strict"
 
-// ##############################################################
-class cScramblerOpParam{
-	name = null /** @type {string} */
-	max = null /** @type {number} */
-	bits = null /** @type {number} */
-
-	/**
-	 *
-	 * @param {string} psName
-	 * @param {number} piMax
-	 */
-	constructor(psName, piMax){
-		this.name = psName
-		this.max = piMax
-		this.bits = cCommon.intBitSize(piMax)
-	}
-}
-
-// ##############################################################
-class cScramblerOp{
-	id = null	/** @type {number} */
-	name = null /** @type {string} */
-	params = null /** @type {Array<cScramblerOpParam>} */
-
-	/**
-	 *
-	 * @param {number} piID
-	 * @param {string} psName
-	 * @param {Array<cScramblerOpParam>} paParams
-	 */
-	constructor(piID, psName, paParams){
-		this.id = piID
-		this.name = psName
-
-		this.params = paParams
-	}
-}
-
+//############################################################
+//# initialise the operations and their parameters.
+//############################################################
 /**
  * The operations that can be applied to the grid. These are the building blocks of the scrambler,
+ * TODO:they are simple (to show the concept) and could be replaced by more complex operations in the future.
+ *
  * and are used to generate the scrambling sequence.
  *  Line – cells move along a line
  * Translate - a row or a column
@@ -49,11 +16,8 @@ class cScramblerOp{
  * 	reflection – a block of cells flipped across an axis
  * 	transpose – swap a row with a column
  * 	skew – shift all rows or columns according to their index
- * 	block – apply the transforms to a defined block within the grid
+ * 	TODO: block – apply the transforms to a defined block within the grid
  */
-const MAX_OP_ID=7
-const OP_ID_BITS = cCommon.intBitSize(MAX_OP_ID)
-
 const OP_IDS = {
 	LINE: {
 		id: 0, name: "line"
@@ -85,6 +49,9 @@ const OP_IDS = {
 	}
 	*/
 }
+const MAX_OP_ID=7
+const OP_ID_BITS = cCommon.intBitSize(MAX_OP_ID)
+
 const OP_PARAMS = {
 	ROWCOL: {
 		id: 0,name: "row or col", max: 1
@@ -93,31 +60,32 @@ const OP_PARAMS = {
 		id: 1,name: "index", max: 200
 	},
 	ROW: {
-		id: 1,name: "row", max: 200
+		id: 2,name: "row", max: 200
 	},
 	COL: {
-		id: 1,name: "col", max: 200
+		id: 3,name: "col", max: 200
 	},
 	DIRECTION: {
-		id: 2,name: "direction", max: 1
+		id: 4,name: "direction", max: 1
 	},
 	DISTANCE: {
-		id: 3,name: "distance", max: 200
+		id: 5,name: "distance", max: 200
 	}
 }
 const OP_STANDARD_PARAMS = [OP_PARAMS.ROWCOL.id, OP_PARAMS.INDEX.id, OP_PARAMS.DIRECTION.id, OP_PARAMS.DISTANCE.id]
 
-const OP_DEFS = [
-	[OP_IDS.LINE.id , OP_STANDARD_PARAMS],
+const OP_DEFS = new Map([
+	[OP_IDS.LINE.id, OP_STANDARD_PARAMS],
 	[OP_IDS.TRANSLATE.id, OP_STANDARD_PARAMS],
-	[OP_IDS.REFLECTION.id , OP_STANDARD_PARAMS],
+	[OP_IDS.REFLECTION.id, OP_STANDARD_PARAMS],
 	[OP_IDS.SQUARE.id, OP_STANDARD_PARAMS],
 	[OP_IDS.UNZIP.id, OP_STANDARD_PARAMS],
 	[OP_IDS.TRANSPOSE.id, [OP_PARAMS.INDEX.id, OP_PARAMS.DISTANCE.id]],
 	[OP_IDS.SKEW.id, [OP_PARAMS.ROWCOL.id, OP_PARAMS.INDEX.id, OP_PARAMS.DIRECTION.id, OP_PARAMS.DISTANCE.id]],
-	[OP_IDS.TRANSLATE_CELL.id, [OP_PARAMS.ROW.id,OP_PARAMS.COL.id,OP_PARAMS.ROW.id,OP_PARAMS.COL.id]],
-]
+	[OP_IDS.TRANSLATE_CELL.id, [OP_PARAMS.ROW.id, OP_PARAMS.COL.id, OP_PARAMS.ROW.id, OP_PARAMS.COL.id]]
+])
 
+//############################################################
 class cScramblerOpReader extends cEventSubscriber{
 	basename = null
 	_ops = null
