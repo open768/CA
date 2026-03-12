d4199574bc4b7b184b4fc1212ce9632a1e8c6e8e
sunil <cluck@chickenkatsu.co.uk>
2026-03-12 21:36:37 +0000

    prep work

--- js/CA/scrambler/op_reader.js
+++ js/CA/scrambler/op_reader.js
@@ -60,7 +60,7 @@ class cOpDefs extends cStaticClass{
 		])
 
 		//---------------------------------------------------------------------
-		this.MAX_OP_ID = this.IDS.SKEW.id
+		this.MAX_OP_ID = cOpConsts.SKEW_OP
 		this.OP_ID_BITS = cCommon.intBitSize(this.MAX_OP_ID)
 
 		//---------------------------------------------------------------------
